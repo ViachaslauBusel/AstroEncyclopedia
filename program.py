249fb5c155906e28eb397603e6701fd6197e402f
Viachaslau <vyacheslav.bysel@gmail.com>
2024-11-17 16:19:29 +0100

    first commit

---
+++ program.py
@@ -0,0 +1,25 @@
+import json
+
+# Загрузка данных из JSON
+def load_data():
+    with open("astronomy_data.json", "r", encoding="utf-8") as file:
+        return json.load(file)
+
+data = load_data()
+
+while True:
+    query = input("Введите запрос: ").strip().lower()
+    if query.lower() == 'exit' or query.lower() == 'выход':  # Прекращение работы цикла
+        print("Выход из программы.")
+        break
+
+    # Поиск информации
+    if_found = False  # Флаг для проверки наличия совпадений
+    for item in data:
+        if query in item[0].lower():  # Поиск подстроки, нечувствительный к регистру
+            if_found = True
+            print(f"{item[0]}: {item[1]}")
+
+    if not if_found:
+        print(f"Информация о '{query}' не найдена.")
+print("Работа программы завершена")
